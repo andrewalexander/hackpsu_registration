6fe45474ce70b32422c2f9b419d143ede3b9598f
Andrew Alexander <andrewalex1992@gmail.com>
2016-01-10 19:50:06 -0500

    Add in some skeleton code

---
+++ scripts.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+# this will be a placeholder for all functions until some better way to organize them makes itself known
+
+
+
+def function():
+    pass
+
+
+def function(self):
+    pass
--- server.py
+++ server.py
@@ -1,22 +1,35 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
-# def index(): pass
 
 @app.route('/login')
 def login(): pass
 
+
 @app.route('/user/<username>')
 def profile(username): pass
+
+
+@app.route('/foo', methods=['GET', 'POST'])
+def foo(x=None, y=None):
+    # do something to send email
+    pass
+# template
+
+# <form action="/foo" method="post">
+#     <button type="submit" value="Send Email" />
+# </form>
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-	
+
 if __name__ == '__main__':
     app.run(debug=True)
     # to make externally visible:
