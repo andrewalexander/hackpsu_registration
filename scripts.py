f3c9beee235e2dfd843d51b739289a045b23818f
Andrew Alexander <andrewalex1992@gmail.com>
2016-01-10 19:59:17 -0500

    More skeleton code

--- scripts.py
+++ scripts.py
@@ -5,9 +5,32 @@ app = Flask(__name__)
 
 
 
-def function():
+def config_db():
     pass
 
 
-def function(self):
+def get_attendees(max_items='25'):
+    pass
+
+
+def generate_unique_url(attendee_id):
+    pass
+
+
+def update_attendee_entries(attendee_list):
+    pass
+
+
+def send_emails(attendee_list):
+    pass
+
+
+def send_alerts(sns_topic):
+    pass
+
+
+# handling user on the rsvp URL
+# assume params will be stripped from request URL
+def get_attendee_from_db(params):
+    attendee_id = params.attendee_id
     pass
