58fec8ed01913319abd14f4145bf281a02a83844
andrewalexander <andrewalex1992@gmail.com>
2016-02-15 16:57:33 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,19 @@
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python2 get-pip.py
+sudo -HE pip install virtualenv
+git clone https://github.com/andrewalexander/hackpsu_registration
+cd hackpsu_registration
+mkdir -p ~/envs/
+virtualenv ~/envs/hackpsu
+source ~/envs/hackpsu/bin/activate
+pip install -r requirements.txt
+sudo -HE brew install node
+sudo -HE npm install -g bower
+sudo -HE npm install 
+python -m SimpleHTTPServer &
+python server.py &
+echo "-----"
+echo "Both servers are running. To quit, type 'fg', then Ctrl+C to"
+echo "exit the 'python server.py' process, and then repeat for the"
+echo "'python -m SimpleHTTPServer' process. When you are finished,"
+echo "type 'deactivate' to exit the virtualenv"
