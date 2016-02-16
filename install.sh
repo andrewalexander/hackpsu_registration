df2310f2b7466a9619ffb8798e102ed0c3b8e417
andrewalexander <andrewalex1992@gmail.com>
2016-02-15 20:30:37 -0500

    More accurate echo at the end

--- install.sh
+++ install.sh
@@ -13,7 +13,10 @@ sudo -HE npm install
 python -m SimpleHTTPServer &
 python server.py &
 echo "-----"
-echo "Both servers are running. To quit, type 'fg', then Ctrl+C to"
-echo "exit the 'python server.py' process, and then repeat for the"
-echo "'python -m SimpleHTTPServer' process. When you are finished,"
-echo "type 'deactivate' to exit the virtualenv"
+echo "Both servers are running. To quit them you will need to manually"
+echo "kill both processes. Use 'ps aux | grep python' to find the Pro-"
+echo "cess ID (PID) of the running process. It is the number in the "
+echo "first column to the right of your user. "
+echo ""
+echo "Kill process with 'kill [PID]', where PID (without brackets) is "
+echo "the Process ID we found above."
